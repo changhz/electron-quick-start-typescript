5b9c7b98552dd0bfdcb7acf43b9acd192cac3087
HZ Chang <hz.chang@pm.me>
2020-02-15 22:17:13 -0500

    update renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+(async () => {
+  const res = await fetch(`https://github.com`);
+  const html = await res.text();
+  console.log(html);
+  const container = document.getElementById("app");
+  container.innerHTML = "";
+})();
